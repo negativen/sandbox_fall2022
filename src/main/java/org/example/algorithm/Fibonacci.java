42e0911c6d8044fa0a1c9594a247f83f4b6fd33c
negativen <wilsnat94@gmail.com>
2022-08-30 19:50:54 -0700

    Fibonacci

---
+++ src/main/java/org/example/algorithm/Fibonacci.java
@@ -0,0 +1,23 @@
+package org.example.algorithm;
+
+public class Fibonacci {
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(0));
+        System.out.println(fib(1));
+        System.out.println(fib(5));
+        System.out.println(fib(10));
+    }
+}
